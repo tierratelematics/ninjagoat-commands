4a0b004abf9e0cebc6540362439949616f72665b
Mattia Franchetto <mt.franchetto@gmail.com>
2017-05-04 18:22:28 +0200

    Export some files

--- scripts/ninjagoat-commands.ts
+++ scripts/ninjagoat-commands.ts
@@ -1,11 +1,10 @@
-import CommandsModule from "./CommandsModule";
 import * as Transport from "./constants/Transport";
 import * as Authentication from "./constants/Authentication";
 import * as CommandDecorators from "./CommandDecorators";
-import CommandDispatcher from "./dispatchers/CommandDispatcher";
-
-export {CommandsModule}
 export {Transport}
 export {Authentication}
 export {CommandDecorators}
-export {CommandDispatcher}
+export {default as CommandModule} from "./CommandsModule";
+export {default as CommandDispatcher} from "./dispatchers/CommandDispatcher";
+export {default as CommandDispatcherEnricher} from "./dispatchers/CommandDispatcherEnricher";
+export {default as PostCommandDispatcher} from "./dispatchers/PostCommandDispatcher";
